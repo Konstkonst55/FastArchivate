b8c48e66f12c764303f8aa932acbf24de8ddec31
Konstkonst55 <77889524+Konstkonst55@users.noreply.github.com>
2022-04-19 01:09:35 +0700

    Добавлена функция архивации

--- app/src/main/java/com/example/fastarchivate/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/fastarchivate/ui/home/HomeFragment.kt
@@ -1,6 +1,6 @@
 package com.example.fastarchivate.ui.home
 
-import android.content.Intent
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +10,29 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.fastarchivate.MyTheme
 import com.example.fastarchivate.databinding.FragmentHomeBinding
 import com.google.android.material.snackbar.Snackbar
+import java.io.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+import androidx.core.app.ActivityCompat
+
+import android.content.pm.PackageManager
+
+import android.net.Uri
+import android.os.Environment
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.fastarchivate.URIHelper
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private val QUERY_CODE = 6969
+    private lateinit var newArchiveName: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,7 +46,8 @@ class HomeFragment : Fragment() {
 
         binding.bCreateArchive.setOnClickListener {
             if (binding.etName.text.toString() != ""){
-                startFilePicker()
+                newArchiveName = binding.etName.text.toString()
+                getContent.launch("*/*")
             }else{
                 Snackbar.make(requireView(), "Enter the archive name", Snackbar.LENGTH_LONG).show()
             }
@@ -41,21 +58,55 @@ class HomeFragment : Fragment() {
 
     private fun init() {
         MyTheme.setTheme(requireContext())
+        verifyStoragePermissions()
     }
 
-    private fun startFilePicker() {
-        val intent = Intent()
-            .setType("*/*")
-            .setAction(Intent.ACTION_GET_CONTENT)
-
-        startActivityForResult(Intent.createChooser(intent, "Select a file"), QUERY_CODE)
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        createZipArchive(URIHelper.getPath(requireContext(), uri!!).toString())
+        Log.i("ZIP", "uri path: " + URIHelper.getPath(requireContext(), uri).toString())
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == QUERY_CODE) {
-            //todo
-            Snackbar.make(requireView(), data?.data?.path.toString(), Snackbar.LENGTH_LONG).show()
+    private fun createZipArchive(fileName: String) {
+        val outPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path.toString() + "/$newArchiveName.zip"
+        val files: Array<String> = arrayOf(fileName)
+        val out = ZipOutputStream(BufferedOutputStream(FileOutputStream(outPath)))
+        Log.i("ZIP", "out")
+        for (file in files) {
+            val fi = FileInputStream(file)
+            val origin = BufferedInputStream(fi)
+            val entry = ZipEntry(file.substring(file.lastIndexOf("/")))
+            out.putNextEntry(entry)
+            origin.copyTo(out, 1024)
+            Log.i("ZIP", "copy_to $outPath")
+            Snackbar.make(requireView(), "The archive is saved in $outPath", Snackbar.LENGTH_LONG)
+                .setAction("COPY") {
+                    val clipboard: ClipboardManager = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                    val clip = ClipData.newPlainText(outPath, outPath)
+                    clipboard.setPrimaryClip(clip)
+                    Snackbar.make(requireView(), "Copied!", Snackbar.LENGTH_LONG).show()
+                }.show()
+            origin.close()
+        }
+        out.close()
+    }
+
+    private fun verifyStoragePermissions() {
+        val REQUEST_EXTERNAL_STORAGE = 1
+        val PERMISSIONS_STORAGE = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        val permission = ActivityCompat.checkSelfPermission(
+            requireActivity(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            )
         }
     }
 
@@ -63,5 +114,4 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
 }
